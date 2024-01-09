ac544839a4eb1e9f38073ea8bd06dbeec786e4fe
Sayed Mahmud <armanemon5801@gmail.com>
2024-01-09 19:25:35 +0600

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* <Route index element={<Home />} />
-        <Route path="blogs" element={<Blogs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} /> */}
-      </Route>
+      <Route path="/" element={<Layout />}></Route>
     </Routes>
   );
 }

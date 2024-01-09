073bcdc19c328f570f5a0e7ea1618fa0b60ccef7
Sayed Mahmud <armanemon5801@gmail.com>
2024-01-09 21:51:50 +0600

    changed api to static

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,11 +12,10 @@ export default function CardsList() {
   const { cardItems, isLoading, formOpen, translate } = useSelector(
     (store) => store.card
   );
-  console.log(cardItems);
   useEffect(() => {
     dispatch(getCardItems());
-  }, []);
-  const itemData = cardItems;
+  }, [dispatch]);
+  // const itemData = [...cardItems, ...itemDataStatic];
   return (
     <Box
       sx={{
@@ -43,15 +42,15 @@ export default function CardsList() {
           {itemData?.map((item) => (
             <ImageListItem key={item.Id}>
               <img
-                srcSet={`${item.ImageUrl}`}
-                src={`${item.ImageUrl}`}
-                alt={item.Name}
+                srcSet={item.img}
+                src={`${item.img}`}
+                alt={item.title}
                 loading="lazy"
                 style={{ borderRadius: "20px" }}
               />
               <ImageListItemBar
                 position="below"
-                title={item.Name}
+                title={item.author}
                 sx={{
                   textAlign: "center",
                   color: "#535353",
@@ -65,3 +64,66 @@ export default function CardsList() {
     </Box>
   );
 }
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    title: "Bed",
+    author: "swabdesign",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    title: "Books",
+    author: "Pavel Nekoranec",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    title: "Sink",
+    author: "Charles Deluvio",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    title: "Kitchen",
+    author: "Christian Mackie",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    title: "Blinds",
+    author: "Darren Richardson",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    title: "Chairs",
+    author: "Taylor Simpson",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    title: "Laptop",
+    author: "Ben Kolde",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    title: "Doors",
+    author: "Philipp Berndt",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    title: "Coffee",
+    author: "Jen P.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    title: "Storage",
+    author: "Douglas Sheppard",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    title: "Candle",
+    author: "Fi Bell",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    title: "Coffee table",
+    author: "Hutomo Abrianto",
+  },
+];
